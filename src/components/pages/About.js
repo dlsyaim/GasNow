27570fff69047c630e4986c70e93af56e152f034
hiepnm <hiepnm@gmail.com>
2017-04-18 15:42:27 +0700

    Locate my location
    	Create locate button
    	Locate using geolocation of RN

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,37 +1,44 @@
 import React from 'react';
 import {
-	View,
 	StyleSheet,
-	Dimensions,
+	Text,
+	View,
+	Platform
 } from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
-const { width, height } = Dimensions.get('window');
 const styles = StyleSheet.create({
 	container: {
-		flex:1,
-		
-		justifyContent: 'flex-end',
+		flex: 1,
+		justifyContent: 'center',
 		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
 	},
-	map: {
-		...StyleSheet.absoluteFillObject,
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5,
 	},
 });
 
 const About = () => (
 	<View style={styles.container}>
-	<MapView
-		style={styles.map}
-		provider={PROVIDER_GOOGLE}
-		initialRegion={{
-			latitude: 37.78825,
-			longitude: -122.4324,
-			latitudeDelta: 0.0922,
-			longitudeDelta: 0.0922 * (width / height),
-		}}
-	/>
+		<Text style={styles.welcome}>
+			This is About Page
+		</Text>
+		<Text style={styles.instructions}>
+			Hello to React-Native
+		</Text>
+		<Text style={styles.instructions}>
+			Press {Platform.OS === 'ios' ? 'Cmd+R' : 'RR'} to reload,{'\n'}
+			{Platform.OS === 'ios' ? 'Cmd+D' : 'Menu Button'} or shake for dev menu
+		</Text>
 	</View>
 );
+
 
 export default About;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,44 +1,118 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {
-	StyleSheet,
-	Text,
 	View,
-	Platform
+	StyleSheet,
+	TouchableOpacity,
+	PixelRatio,
 } from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
-		justifyContent: 'center',
+		flex:1,
+		
+		justifyContent: 'flex-end',
 		alignItems: 'center',
-		backgroundColor: '#F5FCFF',
 	},
-	welcome: {
-		fontSize: 20,
-		textAlign: 'center',
-		margin: 10,
+	map: {
+		...StyleSheet.absoluteFillObject,
 	},
-	instructions: {
-		textAlign: 'center',
-		color: '#333333',
-		marginBottom: 5,
+	btnLocate: {
+		position: 'absolute',
+		left: 10,
+		bottom: 10,
+		height: 25,
+		width: 25,
+		backgroundColor: '#F6F8FA',
+		alignItems: 'center',
+		justifyContent: 'center',
+		borderRadius: 5,
+		borderWidth: 1/PixelRatio.get(),
+		shadowColor: 'black',
+		shadowOffset: {width: 3, height: 3},
+		shadowOpacity: 0.75,
+		shadowRadius: 1,
+	},
+	icLocate: {
+		fontSize: 18,
+		color: 'gray',
 	},
 });
 
-const HomePage = () => (
-	<View style={styles.container}>
-		<Text style={styles.welcome}>
-			This is Home Page
-		</Text>
-		<Text style={styles.instructions}>
-			Hello to React-Native
-		</Text>
-		<Text style={styles.instructions}>
-			Press {Platform.OS === 'ios' ? 'Cmd+R' : 'RR'} to reload,{'\n'}
-			{Platform.OS === 'ios' ? 'Cmd+D' : 'Menu Button'} or shake for dev menu
-		</Text>
-	</View>
-);
+class Home extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			lastPosition: 'unknown',
+			// region: null,
+		}
+		this.watchID = null;
+	}
 
+	locateMe() {
+		const self = this;
+		navigator.geolocation.getCurrentPosition(
+			({coords}) => {
+				const {latitude, longitude} = coords;
+				self.map.animateToRegion({
+					latitude,
+					longitude,
+					latitudeDelta: 0.01,
+					longitudeDelta: 0.002,
+				}, 1000);
+			},
+			(error) => alert(JSON.stringify(error)),
+			{enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+		);
+	}
 
-export default HomePage;
+	watchMe() {
+		this.watchID = navigator.geolocation.watchPosition(
+			(position) => {
+				var lastPosition = JSON.stringify(position);
+				this.setState({lastPosition});
+			}
+		);
+	}
+
+	componentDidMount() {
+		this.locateMe();
+		this.watchMe();
+	}
+
+	componentWillUnmount() {
+		navigator.geolocation.clearWatch(this.watchID);
+	}
+	render() {
+		return (
+			<View style={styles.container}>
+				<MapView
+					ref={map => this.map = map}
+					style={styles.map}
+					provider={PROVIDER_GOOGLE}
+					initialRegion={{
+						latitude: 37.78825,
+						longitude: -122.4324,
+						latitudeDelta: 0.005,
+						longitudeDelta: 0.001,
+					}}
+					followsUserLocation={true}
+					showsMyLocationButton={true}
+					showsUserLocation={true}
+					showCompass={true}
+				/>
+				<TouchableOpacity
+					style={styles.btnLocate}
+					onPress={() => {
+						this.locateMe();
+					}}
+				>
+					<Icon name="md-locate" style={styles.icLocate}/>
+				</TouchableOpacity>
+			</View>
+		);
+	}
+}
+
+export default Home;
